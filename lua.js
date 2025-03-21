c685d52bca3e30fa4fbe9e270ebbbae08837eca6
Amanda Lima <51892828+AmandaLimasiva@users.noreply.github.com>
2025-03-21 16:04:02 -0300

    estilizando o projeto

--- lua.js
+++ lua.js
@@ -3,17 +3,24 @@ import { phase } from "https://cdn.skypack.dev/lune";
 function verificaAFase(date) {
   const lua = phase(date);
   const fase = lua.phase;
+  let nomeFase = "Fase desconhecida";
+  let imagem = "";
 
   if (fase >= 0 && fase < 0.125) {
-    return "Lua Nova";
+    nomeFase = "Lua Nova";
+    imagem = "./imgs/nova.jpg";
   } else if (fase >= 0.125 && fase < 0.5) {
-    return "Lua Crescente";
+    nomeFase = "Lua Crescente";
+    imagem = "./imgs/crescente.jpg";
   } else if (fase >= 0.5 && fase < 0.875) {
-    return "Lua Cheia";
+    nomeFase = "Lua Cheia";
+    imagem = "./imgs/cheia.jpg";
   } else if (fase >= 0.875 && fase < 1) {
-    return "Lua Minguante";
+    nomeFase = "Lua Minguante";
+    imagem = "./imgs/nova.jpg";
   }
-  return "Fase desconhecida";
+
+  return { nomeFase, imagem }; // Retorna um objeto contendo a fase e a imagem
 }
 
 function obterDataHoraBrasilia() {
@@ -31,13 +38,17 @@ function obterDataHoraBrasilia() {
   return formatador.format(agora);
 }
 
+//EXIBE
 function atualizarExibicao() {
   const data = new Date();
-  const fase = verificaAFase(data);
+  const resultado = verificaAFase(data); // Agora resultado é um objeto
+  const nomeFase = resultado.nomeFase; // Pega apenas o nome da fase
+  const imagem = resultado.imagem; // Pega a imagem correspondente
   const dataHora = obterDataHoraBrasilia();
 
-  document.getElementById("faseLua").textContent = `Fase da Lua: ${fase}`;
+  document.getElementById("faseLua").textContent = `Fase da Lua: ${nomeFase}`;
   document.getElementById("dataHora").textContent = `Data e Hora em Brasília: ${dataHora}`;
+  document.getElementById("imgLua").src = imagem;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
